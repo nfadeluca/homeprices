8198186d7fa50f1be9d88c50a79c65eb55da7c0b
nfadeluca <nfadeluca@users.noreply.github.com>
2020-11-04 20:00:04 -0500

    Update homeprices.py

--- homeprices.py
+++ homeprices.py
@@ -1,2 +1,10 @@
-print("Hello World")
-# hello there
+import quandl
+import matplotlib.pyplot as plt
+
+# This authenticates your account so that you may use data from Quandl
+# You can find your authentication key in your Quandl account settings
+quandl.ApiConfig.api_key = "s6x8gXSardesDec_nbyH"
+
+# This is how we plot our x's and y's
+plt.plot([1,2,3],[5,7,4])
+plt.show()
