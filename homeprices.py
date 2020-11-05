46a2bbb5412628558aa07f7f129b0edb01eac9ff
nfadeluca <nfadeluca@users.noreply.github.com>
2020-11-04 20:24:04 -0500

    Update homeprices.py
    testing the use of getting zillow data and using it with python

--- homeprices.py
+++ homeprices.py
@@ -1,10 +1,15 @@
 import quandl
 import matplotlib.pyplot as plt
 
+
 # This authenticates your account so that you may use data from Quandl
 # You can find your authentication key in your Quandl account settings
 quandl.ApiConfig.api_key = "s6x8gXSardesDec_nbyH"
 
-# This is how we plot our x's and y's
+# This is how we plot our x's and y's using matplotlib
 plt.plot([1,2,3],[5,7,4])
 plt.show()
+
+# Making use of Zillow data
+data = quandl.get_table('ZILLOW/DATA', indicator_id='ZSFH', region_id='99999')
+print(data)
