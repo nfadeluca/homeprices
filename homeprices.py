d18a444e287641d10ece116f632569aa30e5c61c
Max <maxbabka8@gmail.com>
2020-11-14 15:26:14 -0500

    Put parsing data into method so it's easier to run

--- homeprices.py
+++ homeprices.py
@@ -1,26 +1,27 @@
+#!/usr/bin/env python
 import quandl
 import matplotlib.pyplot as plt
 import pandas as pd
+""""""
 
 
 
 # Making use of Zillow data
-# This code uses the mass file
-print("Reading file")
-df = pd.read_csv("ZILLOW_DATA.csv")
-print("File loaded!")
-df = df.copy()[df['date'] == '2020-01-31']
-df.to_csv("ZILLOW_JAN.csv", index = False)
-print("File copied!")
-df = df.copy()[df['indicator_id'] == 'ZATT']
-df.to_csv("ZILLOW_ZATT.csv", index = False)
-print("File copied!")
+"""
+This method reads the Zillow data file and then outputs a csv file containing
+only the home price data for a certain month and type of home
+"""
+def pullDataSubset():
+    print("Reading file")
+    df = pd.read_csv("ZILLOW_DATA.csv")
+    print("File loaded!")
+    #The date here is the date that will be outputted
+    df = df.copy()[df['date'] == '2020-01-31']
+    #index = False gets rid of the first column
+    df.to_csv("ZILLOW_JAN.csv", index = False)
+    print("File copied!")
+    #This
+    df = df.copy()[df['indicator_id'] == 'ZATT']
+    df.to_csv("ZILLOW_ZATT.csv", index = False)
+    print("File copied!")
 
-
-# print(list(df.columns))
-#print(df['date'].head())
-
-
-
-# print(df.head(10))
-# print(df.size)
