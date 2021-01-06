c3117f685c3dd573d5adfb8d307c6df30c3f9b97
nfadeluca <71537945+nfadeluca@users.noreply.github.com>
2021-01-06 18:16:06 -0500

    Working with Geopandas

--- homeprices.py
+++ homeprices.py
@@ -2,7 +2,8 @@
 import quandl
 import matplotlib.pyplot as plt
 import pandas as pd
-
+import numpy
+import geopandas as gpd 
 
 
 # Making use of Zillow data
@@ -36,5 +37,34 @@ def sortData():
     df = df.sort_values(by='value')
     return df
 
-sortData()['value'].plot(kind='hist', bins=100, grid=True, range=[0, 1000000])
-plt.show()
+#sortData()['value'].plot(kind='hist', bins=100, grid=True, range=[0, 1000000])
+#plt.show()
+
+'''
+activate myenv to work with conda (for nick on visual studio)
+'''
+
+file_path = 'C:/Users/nfade/Documents/GitHub/homeprices/map/tl_2019_us_zcta510.shx'
+map_us = gpd.read_file(file_path)
+
+'''
+ZCTA5CE10       object
+GEOID10         object
+CLASSFP10       object
+MTFCC10         object
+FUNCSTAT10      object
+ALAND10          int64
+AWATER10         int64
+INTPTLAT10      object
+INTPTLON10      object
+geometry      geometry
+'''
+
+
+fig, ax = plt.subplots(1, figsize=(15, 8))
+plt.title('Map of the United States Sub-Regions', size=16)
+map_us[(map_us['ALAND10']) & (map_us['ALAND10'])].plot(column='ZCTA5CE10',                                                    
+             cmap='Greens',      # Colormap for the states                     
+             linewidth=0.4,      # line width for state borders
+             ax=ax,              # plotting the map on 'ax'
+             edgecolor='black'); # State border colors
